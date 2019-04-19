79d5f5753c526ddc4e57bc8a204423d46945e59e
YanYongSheng <787383617@qq.com>
2019-04-19 15:39:25 +0800

    Python：map/reduce

--- venv/Study/listComprehensions.py
+++ venv/Study/listComprehensions.py
@@ -12,28 +12,29 @@ print(L)
 
 # 但是循环太繁琐，而列表生成式则可以用一行语句代替循环生成上面的list：
 print([x * x for x in range(1, 11)])
-#for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
-print( [x * x for x in range(1, 11) if x % 2 == 0])
-#还可以使用两层循环，可以生成全排列：
+# for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
+print([x * x for x in range(1, 11) if x % 2 == 0])
+# 还可以使用两层循环，可以生成全排列：
 print([m + n for m in 'ABC' for n in 'XYZ'])
 
-#运用列表生成式，可以写出非常简洁的代码。例如，列出当前目录下的所有文件和目录名，可以通过一行代码实现：
-import os # 导入os模块
-print([d for d in os.listdir('.')]) # os.listdir可以列出文件和目录
+# 运用列表生成式，可以写出非常简洁的代码。例如，列出当前目录下的所有文件和目录名，可以通过一行代码实现：
+import os  # 导入os模块
 
-#for循环其实可以同时使用两个甚至多个变量，比如dict的items()可以同时迭代key和value：
-d = {'x': 'A', 'y': 'B', 'z': 'C' }
-for k,v in d.items():
-    print(k,'=',v)
-#因此，列表生成式也可以使用两个变量来生成list：
-d = {'x': 'A', 'y': 'B', 'z': 'C' }
-print([k+"="+v for k,v in d.items()])
-#最后把一个list中所有的字符串变成小写：
+print([d for d in os.listdir('.')])  # os.listdir可以列出文件和目录
+
+# for循环其实可以同时使用两个甚至多个变量，比如dict的items()可以同时迭代key和value：
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+for k, v in d.items():
+    print(k, '=', v)
+# 因此，列表生成式也可以使用两个变量来生成list：
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+print([k + "=" + v for k, v in d.items()])
+# 最后把一个list中所有的字符串变成小写：
 L = ['Hello', 'World', 'IBM', 'Apple']
 print([s.lower() for s in L])
 
 L1 = ['Hello', 'World', 18, 'Apple', None]
-L2 = [s.lower() for s in L1 if isinstance(s,str)]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
 print(L2)
 
 # 测试:
