c73a0d6c4d123bae2cc6233355356384f3dc7e68
TheagreedDreaIsland <787383617@qq.com>
2019-04-24 17:07:14 +0800

    Python：map和reduce

--- venv/Study/map_and_reduce.py
+++ venv/Study/map_and_reduce.py
@@ -51,3 +51,84 @@ print(list(map(str,[1,2,3,4,5,6,7,8,9])))
 #再看reduce的用法。reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，
 # 这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算，其效果就是：
 
+#比方说对一个序列求和，就可以用reduce实现
+from functools import reduce
+def add(x,y):
+    return x+y
+print(reduce(add,[1, 3, 5, 7, 9]))
+
+# 当然求和运算可以直接用Python内建函数sum()，没必要动用reduce。
+
+# 但是如果要把序列[1, 3, 5, 7, 9]变换成整数13579，reduce就可以派上用场：
+def fn(x,y):
+    return x*10+y
+print(reduce(fn,[1,3,5,7,9]))
+
+#这个例子本身没多大用处，但是，如果考虑到字符串str也是一个序列，
+# 对上面的例子稍加改动，配合map()，我们就可以写出把str转换为int的函数：
+def char2num(s):
+    digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    return digits[s]
+print(reduce(fn,map(char2num,'13579')))
+
+#整理成一个str2int的函数就是：
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
+
+#可以用lambda函数进一步简化成：
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def char2num(s):
+    return DIGITS[s]
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+#也就是说，假设Python没有提供int()函数，你完全可以自己写一个把字符串转化为整数的函数，而且只需要几行代码！
+
+#lambda函数的用法在后面介绍。
+
+#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# 输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+def normalize(name):
+    return name.upper()[:1] + name.lower()[1:]
+# 测试:
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+#Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+def prod(L):
+    return reduce(lambda x,y:x*y,L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+def str2float(s):
+    index = s.find('.')
+    if index >= 0:
+        s = s.replace('.', '')
+
+    def fn(x, y):
+        return 10 * x + y
+
+    def charNum(s):
+        return DIGITS[s]
+
+    ss = reduce(fn, map(charNum, s))
+    # 找到小数点的位置，除以10的n次方
+    ss /= pow(10, len(s) - index)
+    return ss
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
