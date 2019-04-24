01cbce9c0a96f72d4452f5fe4322d1af7f090f3d
TheagreedDreaIsland <787383617@qq.com>
2019-04-24 17:18:24 +0800

    Python：map和reduce

--- venv/Study/map_and_reduce.py
+++ venv/Study/map_and_reduce.py
@@ -30,7 +30,8 @@
 def f(x):
     return x * x
 
-r=map(f,[1,2,3,4,5,6,7,8,9])
+
+r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(list(r))
 
 # map()传入的第一个参数是f，即函数对象本身。由于结果r是一个Iterator，
@@ -38,80 +39,104 @@ print(list(r))
 
 # 你可能会想，不需要map()函数，写一个循环，也可以计算出结果：
 L = []
-for x in [1,2,3,4,5,6,7,8,9]:
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
     L.append(f(x))
 print(L)
-#的确可以，但是，从上面的循环代码，能一眼看明白“把f(x)作用在list的每一个元素并把结果生成一个新的list”吗？
+# 的确可以，但是，从上面的循环代码，能一眼看明白“把f(x)作用在list的每一个元素并把结果生成一个新的list”吗？
 
-#所以，map()作为高阶函数，事实上它把运算规则抽象了，因此，我们不但可以计算简单的f(x)=x2，
+# 所以，map()作为高阶函数，事实上它把运算规则抽象了，因此，我们不但可以计算简单的f(x)=x2，
 # 还可以计算任意复杂的函数，比如，把这个list所有数字转为字符串：
-print(list(map(str,[1,2,3,4,5,6,7,8,9])))
-#只需要一行代码
+print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+# 只需要一行代码
 
-#再看reduce的用法。reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，
+# 再看reduce的用法。reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，
 # 这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算，其效果就是：
 
-#比方说对一个序列求和，就可以用reduce实现
+# 比方说对一个序列求和，就可以用reduce实现
 from functools import reduce
-def add(x,y):
-    return x+y
-print(reduce(add,[1, 3, 5, 7, 9]))
+
+
+def add(x, y):
+    return x + y
+
+
+print(reduce(add, [1, 3, 5, 7, 9]))
+
 
 # 当然求和运算可以直接用Python内建函数sum()，没必要动用reduce。
 
 # 但是如果要把序列[1, 3, 5, 7, 9]变换成整数13579，reduce就可以派上用场：
-def fn(x,y):
-    return x*10+y
-print(reduce(fn,[1,3,5,7,9]))
+def fn(x, y):
+    return x * 10 + y
 
-#这个例子本身没多大用处，但是，如果考虑到字符串str也是一个序列，
+
+print(reduce(fn, [1, 3, 5, 7, 9]))
+
+
+# 这个例子本身没多大用处，但是，如果考虑到字符串str也是一个序列，
 # 对上面的例子稍加改动，配合map()，我们就可以写出把str转换为int的函数：
 def char2num(s):
     digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
     return digits[s]
-print(reduce(fn,map(char2num,'13579')))
 
-#整理成一个str2int的函数就是：
+
+print(reduce(fn, map(char2num, '13579')))
+
+# 整理成一个str2int的函数就是：
 DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
 
 def str2int(s):
     def fn(x, y):
         return x * 10 + y
+
     def char2num(s):
         return DIGITS[s]
+
     return reduce(fn, map(char2num, s))
 
-#可以用lambda函数进一步简化成：
+
+# 可以用lambda函数进一步简化成：
 DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
 
 def char2num(s):
     return DIGITS[s]
 
+
 def str2int(s):
     return reduce(lambda x, y: x * 10 + y, map(char2num, s))
 
-#也就是说，假设Python没有提供int()函数，你完全可以自己写一个把字符串转化为整数的函数，而且只需要几行代码！
 
-#lambda函数的用法在后面介绍。
+# 也就是说，假设Python没有提供int()函数，你完全可以自己写一个把字符串转化为整数的函数，而且只需要几行代码！
 
-#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# lambda函数的用法在后面介绍。
+
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
 # 输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
 def normalize(name):
     return name.upper()[:1] + name.lower()[1:]
+
+
 # 测试:
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
 print(L2)
 
-#Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
 def prod(L):
-    return reduce(lambda x,y:x*y,L)
+    return reduce(lambda x, y: x * y, L)
+
+
 print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
 if prod([3, 5, 7, 9]) == 945:
     print('测试成功!')
 else:
     print('测试失败!')
-#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
 def str2float(s):
     index = s.find('.')
     if index >= 0:
@@ -127,6 +152,8 @@ def str2float(s):
     # 找到小数点的位置，除以10的n次方
     ss /= pow(10, len(s) - index)
     return ss
+
+
 print('str2float(\'123.456\') =', str2float('123.456'))
 if abs(str2float('123.456') - 123.456) < 0.00001:
     print('测试成功!')
